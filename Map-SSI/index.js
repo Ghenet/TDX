8e3fde452c03544e62c79ef6c01b2d7253e6f371
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2023-08-02 11:16:55 -0700

    refreshes the getIss call every 1min automatically

--- Map-SSI/index.js
+++ Map-SSI/index.js
@@ -1,5 +1,11 @@
-//creates the map using leaflet js
+//creates the map using leaflet js and tiles using openstreet map api
+   
     const map = L.map('map').setView([0, 0], 2);
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contibutors'
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    const tiles = L.tileLayer(tileUrl, {attribution});
+    tiles.addTo(map);
+//making a marker with a custom icon
     const issIcon = L.icon({
         iconUrl: 'SpaceStation.png',
         iconSize: [30, 22],
@@ -7,11 +13,7 @@
         
     });
     const marker = L.marker([0, 0],{icon: issIcon}).addTo(map);
-//use street map api for tiles
-    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contibutors'
-    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    const tiles = L.tileLayer(tileUrl, {attribution});
-    tiles.addTo(map);
+
 //iss api call
     const iss_api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
@@ -22,6 +24,7 @@
 
             // L.marker([latitude, longitude]).addTo(map);
             marker.setLatLng([latitude, longitude]);
+            map.setView([latitude, longitude],7); //zooms the map to 4 where the ISS is in the specified LatLng
 
             document.getElementById('lat').textContent = latitude;
             document.getElementById('lon').textContent = longitude;
@@ -32,3 +35,5 @@
             
         }
          getISS()
+
+          setInterval(getISS, 1000)
