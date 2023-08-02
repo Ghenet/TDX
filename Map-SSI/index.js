ad59d8f8ccc18850772d01d68785d1251e3efd72
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2023-08-01 21:04:47 -0700

    update two dom elements with Latitude and Longitude data

--- Map-SSI/index.js
+++ Map-SSI/index.js
@@ -1,1 +1,18 @@
-hello 
+
+
+    const iss_api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+    async function getISS() {
+            const response = await fetch(iss_api_url);
+            const data = await response.json();
+            const { latitude, longitude } = data;
+
+            document.getElementById('lat').textContent = latitude;
+            document.getElementById('lon').textContent = longitude;
+
+            console.log(data);
+            console.log(data.latitude);
+            console.log(data.longitude);
+            
+        }
+        // getISS()
